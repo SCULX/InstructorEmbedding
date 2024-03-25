71e567dafefcd00f07bf72766556b2e0d2a03ab9
LX <3620846907@qq.com>
2024-03-25 10:30:43 +0800

    检查了模型参数,确认加载了微调之后的模型参数,证明结果的正确

--- Evaluation/bert_base_fintune_mteb.py
+++ Evaluation/bert_base_fintune_mteb.py
@@ -2,18 +2,13 @@
 
 from mteb import MTEB
 from sentence_transformers import SentenceTransformer, models
-import sentence_transformers
-from transformers import BertConfig, BertForSequenceClassification
-from transformers import BertConfig, BertModel
-
-
-# 加载微调后模型配置
-config = BertConfig.from_pretrained('results/checkpoint-1500', output_hidden_states=False)  
-
-# 加载微调后的BERT-base模型,不包含分类头
-bert_model = BertModel.from_pretrained('results/checkpoint-1500', config=config)
+from transformers import BertForSequenceClassification
+from transformers import BertConfig, BertModel,BertTokenizer
 
 # 创建一个编码器，它是整个transformer模型（BERT）
+# 因为他只加载编码word的部分，所以在当前符合想要地结果
+# 且我严格对比了微调之后的bertforclassification的bert参数和这个word_embedding_model中参数，参数是完全相等的,并且
+# word_embedding_model中确实只有bert-base-uncased这个模型的参数结构,只是名字变化了,但确实不含bertforclassfication中多余的二分类那层的参数
 word_embedding_model = models.Transformer('results/checkpoint-1500')
 
 # 创建一个池化层，池化层使用和原始bert-base-uncased模型一样的平均池化
@@ -24,6 +19,7 @@ pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension
 
 # 创建一个SentenceTransformer实例
 model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+
 
 
 evaluation = MTEB(tasks=["Banking77Classification"])
